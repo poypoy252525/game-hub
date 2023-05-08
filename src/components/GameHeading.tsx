336b79dcc38621434bc15979d72445c7c4aa2c57
poypoy252525 <61503297+poypoy252525@users.noreply.github.com>
2023-05-09 01:35:59 +0800

    creating hook for single platform and genre

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -1,19 +1,15 @@
 import { Title } from "@mantine/core";
 import { GameQuery } from "./../App";
-import usePlatforms from "../hooks/usePlatforms";
-import useGenres from "../hooks/useGenres";
+import usePlatform from "../hooks/usePlatform";
+import useGenre from "../hooks/useGenre";
 
 interface Props {
   gameQuery: GameQuery;
 }
 
 const GameHeading = ({ gameQuery }: Props) => {
-  const { data: platforms } = usePlatforms();
-  const platform = platforms?.results.find(
-    (platform) => platform.id === gameQuery.platformId
-  );
-  const { data: genres } = useGenres();
-  const genre = genres?.results.find((genre) => genre.id === gameQuery.genreId);
+  const platform = usePlatform(gameQuery.platformId);
+  const genre = useGenre(gameQuery.genreId);
   const heading = `${platform?.name || ""} ${genre?.name || ""} Games`;
   return (
     <Title order={1} my={5}>

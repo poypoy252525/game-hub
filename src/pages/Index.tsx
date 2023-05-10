b29ec6d827ef0fab36dacfbaf3318ad2dfae05a9
poypoy252525 <61503297+poypoy252525@users.noreply.github.com>
2023-05-11 01:19:15 +0800

    makeing responsive

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Group } from "@mantine/core";
+import { Box, Grid, Group, MediaQuery } from "@mantine/core";
 import GenreList from "../components/GenreList";
 import GameGrid from "../components/GameGrid";
 import GameHeading from "../components/GameHeading";
@@ -9,16 +9,17 @@ const Index = () => {
   return (
     <Box p={16}>
       <Grid>
-        <Grid.Col md={2}>
-          <GenreList />
-        </Grid.Col>
-        <Grid.Col md={10}>
+        <MediaQuery smallerThan="md" styles={{ display: "none" }}>
+          <Grid.Col md={3} lg={2}>
+            <GenreList />
+          </Grid.Col>
+        </MediaQuery>
+        <Grid.Col md={9} lg={10}>
           <GameHeading />
           <Group mb="md" noWrap>
             <PlatformSelector />
             <SortSelector />
           </Group>
-
           <GameGrid />
         </Grid.Col>
       </Grid>
